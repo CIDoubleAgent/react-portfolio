2b4c5856562d76752a0bd9e1eb420f9c07f00c9b
Nick Zabkar <nzabkar51@gmail.com>
2021-11-11 19:08:30 -0500

    Added Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Navigation () {
+    return (
+        <nav class="navigation col 12 grey lighten-2">  
+        <a id="small-text" class="col 4 light-blue-text text-darken-3" href="#about-me">
+            &nbsp;&nbsp; About Me &nbsp;
+        </a>
+    
+        <a id="small-text" class="col 4 light-blue-text text-darken-3" href="#work">
+            &nbsp; Portfolio &nbsp;
+        </a>
+    
+        <a id="small-text" class="col 4 light-blue-text text-darken-3" href="#contact-me">
+            &nbsp; Contact &nbsp;
+        </a>
+
+        <a id="small-text" class="col 4 light-blue-text text-darken-3" href="#contact-me">
+            &nbsp; Resume &nbsp;
+        </a> 
+    </nav>
+    )
+}
+
+export default Navigation
