0078154c651eeb3528a1425faae5273612669f6f
Nick Zabkar <nzabkar51@gmail.com>
2021-11-11 19:07:17 -0500

    Deleted standard code in App function & replaced

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Navigation />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Footer />
     </div>
   );
 }
