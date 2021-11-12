b3a99ab272c9dfae2cb11f38be0cd21f3d848860
Nick Zabkar <nzabkar51@gmail.com>
2021-11-11 19:07:59 -0500

    Added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+        return (
+            
+            <footer class="page-footer light-blue darken-3">
+                <div id="page-footer" class="container center-align">
+                © 2021 Nicholas Zabkar
+                </div>
+            </footer>
+          
+        )
+    
+}
+
+export default Footer
