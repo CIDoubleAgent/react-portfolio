c5d37f4ca5b7a12199de6378e85ef5dd9b2eba1f
Nick Zabkar <nzabkar51@gmail.com>
2021-11-11 19:47:39 -0500

    Linked AboutME on navbar to "/"

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,19 +3,19 @@ import React from 'react';
 function Navigation () {
     return (
         <nav class="navigation col 12 grey lighten-2">  
-        <a id="small-text" class="col 4 light-blue-text text-darken-3" href="#about-me">
+        <a id="small-text" class="col 4 light-blue-text text-darken-3" href="/">
             &nbsp;&nbsp; About Me &nbsp;
         </a>
     
-        <a id="small-text" class="col 4 light-blue-text text-darken-3" href="#work">
+        <a id="small-text" class="col 4 light-blue-text text-darken-3">
             &nbsp; Portfolio &nbsp;
         </a>
     
-        <a id="small-text" class="col 4 light-blue-text text-darken-3" href="#contact-me">
+        <a id="small-text" class="col 4 light-blue-text text-darken-3">
             &nbsp; Contact &nbsp;
         </a>
 
-        <a id="small-text" class="col 4 light-blue-text text-darken-3" href="#contact-me">
+        <a id="small-text" class="col 4 light-blue-text text-darken-3">
             &nbsp; Resume &nbsp;
         </a> 
     </nav>
