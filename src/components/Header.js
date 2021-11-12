21848de4e64ba701cff6746e41ca822d78855022
Nick Zabkar <nzabkar51@gmail.com>
2021-11-11 19:08:13 -0500

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+        return (
+            <header class="row grey darken-3">
+                <div id="banner">
+                    <h2 class="light-blue-text">
+                    Nicholas Zabkar's
+                    </h2>
+            
+                    <h4>
+                    <span id="subtitle" class="right grey-text text-lighten-4">Web Developer Portfolio</span>
+                    </h4>
+                </div>
+            </header>
+        )
+    
+}
+
+export default Header
