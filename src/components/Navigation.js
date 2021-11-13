6039c1bc2cc80c0478f2bfa5db7c88f25f7f0766
Nick Zabkar <nzabkar51@gmail.com>
2021-11-13 10:02:26 -0500

    cleaned up code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
-function Navigation ({ handlePageChange }) {
+function Navigation({ handlePageChange }) {
     return (
-        <nav class="navigation col 12 grey lighten-2">  
-        <button id="small-text" class="navbtn btn col 4 light-blue darken-3" onClick={() => handlePageChange('AboutMe')}>
-            About Me
-        </button>
-    
-        <button id="small-text" class="navbtn btn col 4 light-blue darken-3" onClick={() => handlePageChange('Portfolio')}>
-            Portfolio
-        </button>
-    
-        <button id="small-text" class="navbtn btn col 4 light-blue darken-3" onClick={() => handlePageChange('Contact')}>
-            Contact
-        </button>
+        
+            <nav class="navigation col 12 grey lighten-2">
+                <button id="small-text" class="navbtn btn col 4 light-blue darken-3" onClick={() => handlePageChange('AboutMe')}>
+                    About Me
+                </button>
 
-        <a id="small-text" class="navbtn btn col 4 light-blue darken-3" href="https://docs.google.com/document/d/1IHctUDXitDshLIJZ_pgVyyoPuoAp94CtnxQC489VYgs/edit?usp=sharing">
-            Resume
-        </a> 
-        </nav>
+                <button id="small-text" class="navbtn btn col 4 light-blue darken-3" onClick={() => handlePageChange('Portfolio')}>
+                    Portfolio
+                </button>
+
+                <button id="small-text" class="navbtn btn col 4 light-blue darken-3" onClick={() => handlePageChange('Contact')}>
+                    Contact
+                </button>
+
+                <a id="small-text" class="navbtn btn col 4 light-blue darken-3" href="https://docs.google.com/document/d/1IHctUDXitDshLIJZ_pgVyyoPuoAp94CtnxQC489VYgs/edit?usp=sharing">
+                    Resume
+                </a>
+            </nav>
+
     )
 }
 
