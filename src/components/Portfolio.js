8f725ec594861db4d215a8779c6ef37ed4dd945a
Nick Zabkar <nzabkar51@gmail.com>
2022-03-26 11:29:31 -0400

    Fixed project links and changed styling

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,97 +10,97 @@ function Portfolio() {
 
             <section class="row">
                 <article class="col s12 m8 offset-m2 l5 offset-l1">
-                    <div class="card hoverable">
+                    <div class="project-card cyan darken-3 card hoverable">
+                    <h5 class="card-title white-text">Crypt-ID</h5>
                         <div class="card-image">
                             <img id="thumbnail" class="responsive-img" src="https://github.com/CIDoubleAgent/Nicks-Web-Dev-Portfolio/blob/main/images/Crypt-ID_1.png?raw=true" alt="Crypt-Id" />
                         </div>
-                        <span class="card-title light-blue-text text-darken-3">Crypt-ID</span>
-                        <div class="card-content">
+                        <div class="card-content white-text">
                             <p>Crypt-ID was designed for loging your strange experiences and reading about other peoples encounters.</p>
                         </div>
                         <div class="card-action">
-                            <a href="https://sleepy-island-20575.herokuapp.com/">Deployed Application</a>
-                            <a class="right" href="https://github.com/CIDoubleAgent/Crypt---ID">GitHub Repository</a>
+                            <a href="https://sleepy-island-20575.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                            <a class="right" href="https://github.com/CIDoubleAgent/Crypt-ID" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
                         </div>
                     </div>
                 </article>
                 
                 <article class="col s12 m8 offset-m2 l5">
-                    <div class="card hoverable">
+                    <div class="project-card cyan darken-3 card hoverable">
+                    <h5 class="card-title white-text">Kan(way)e</h5>
                         <div class="card-image">
                             <img id="thumbnail" class="responsive-img" src="https://github.com/CIDoubleAgent/Nicks-Web-Dev-Portfolio/blob/main/images/Kan(way)e_1.png?raw=true" alt="Kanwaye" />
                         </div>
-                        <span class="card-title light-blue-text text-darken-3">Kan(way)e</span>
-                        <div class="card-content">
+                        <div class="card-content white-text">
                             <p>An app for finding new music by genre, or alternatively just Kanye West songs.</p>
                         </div>
                         <div class="card-action">
-                            <a href="https://kelley-mcd.github.io/STANK-Music/">Deployed Application</a>
-                            <a class="right" href="https://github.com/CIDoubleAgent/STANK-Music">GitHub Repository</a>
+                            <a href="https://kelley-mcd.github.io/STANK-Music/" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                            <a class="right" href="https://github.com/CIDoubleAgent/STANK-Music" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
                         </div>
                     </div>
                 </article>
 
                 <article class="col s12 m8 offset-m2 l5 offset-l1">
-                    <div class="card hoverable">
+                    <div class="project-card cyan darken-3 card hoverable">
+                    <h5 class="card-title white-text">Weather Dashboard</h5>
                         <div class="card-image">
                             <img id="thumbnail" class="responsive-img" src="https://github.com/CIDoubleAgent/Nicks-Web-Dev-Portfolio/blob/main/images/Weather%20Dash_2.png?raw=true" alt="Weather-Dashboard" />
                         </div>
-                        <span class="card-title light-blue-text text-darken-3">Weather Dashboard</span>
-                        <div class="card-content card-content-weather">
+                        <div class="card-content white-text card-content-weather">
                             <p>A weather app with current as well as five day forecasts. Displays weather data for a selected city.</p>
                         </div>
                         <div class="card-action">
-                            <a href="https://cidoubleagent.github.io/weather-dashboard/">Deployed Application</a>
-                            <a class="right" href="https://github.com/CIDoubleAgent/weather-dashboard">GitHub Repository</a>
+                            <a href="https://cidoubleagent.github.io/weather-dashboard/" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                            <a class="right" href="https://github.com/CIDoubleAgent/weather-dashboard" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
                         </div>
                     </div>
                 </article>
 
                 <article class="col s12 m8 offset-m2 l5">
-                    <div class="card hoverable">
+                    <div class="project-card cyan darken-3 card hoverable">
+                    <h5 class="card-title white-text">Note Taker</h5>
                         <div class="card-image">
                             <img id="thumbnail" class="responsive-img" src="https://github.com/CIDoubleAgent/Nicks-Web-Dev-Portfolio/blob/main/images/Note_Taker_3.png?raw=true" alt="Note-Taker" />
                         </div>
-                        <span class="card-title light-blue-text text-darken-3">Note Taker</span>
-                        <div class="card-content">
+                        <div class="card-content white-text">
                             <p>A note taker App designed for simplicity, clarity and convenience. Add or delete your notes.</p>
                         </div>
                         <div class="card-action">
-                            <a href="https://lit-fortress-73592.herokuapp.com/notes">Deployed Application</a>
-                            <a class="right" href="https://github.com/CIDoubleAgent/Note-Taker">GitHub Repository</a>
+                            <a href="https://lit-fortress-73592.herokuapp.com/notes" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                            <a class="right" href="https://github.com/CIDoubleAgent/Note-Taker" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
                         </div>
                     </div>
                 </article>
 
                 <article class="col s12 m8 offset-m2 l5 offset-l1">
-                    <div class="card hoverable">
+                    <div class="project-card cyan darken-3 card hoverable">
+                    <h5 class="card-title white-text">App.Fix()</h5>
                         <div class="card-image">
                             <img id="thumbnail" class="responsive-img" src="https://github.com/CIDoubleAgent/react-portfolio/blob/main/src/components/images/app.fix_1.png?raw=true" alt="App.Fix" />
-                        </div>
-                        <span class="card-title light-blue-text text-darken-3">App.Fix()</span>
-                        <div class="card-content">
+                        </div>                  
+                        <div class="card-content white-text">
                             <p>App.Fix is a tech blog app where people can post their tech issues and comment suggestions for solutions.</p>
                         </div>
                         <div class="card-action">
-                            <a href="https://obscure-citadel-44297.herokuapp.com/">Deployed Application</a>
-                            <a class="right" href="https://github.com/CIDoubleAgent/App.Fix">GitHub Repository</a>
+                            <a href="https://polar-refuge-49734.herokuapp.com/" target="_blank" rel="noopener noreferrer">Deployed Application</a>
+                            <a class="right" href="https://github.com/CIDoubleAgent/App.Fix" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
                         </div>
                     </div>
                 </article>
 
                 <article class="col s12 m8 offset-m2 l5">
-                    <div class="card hoverable">
+                    <div class="project-card cyan darken-3 card hoverable">
+                    <h5 class="card-title white-text">My Team Profile Generator</h5>
                         <div class="card-image">
                             <img id="thumbnail" class="responsive-img" src="https://github.com/CIDoubleAgent/react-portfolio/blob/main/src/components/images/team-profile-generator.png?raw=true" alt="Team Profile Generator" />
                         </div>
-                        <span class="card-title light-blue-text text-darken-3">My Team Profile Generator</span>
-                        <div class="card-content">
+                        <div class="card-content white-text">
                             <p>A back end application that generates a team profile based on user input.</p>
                         </div>
                         <div class="card-action">
-                            <a href="/">Deployed Application</a>
-                            <a class="right" href="https://github.com/CIDoubleAgent/My-Team-Profile-Generator">GitHub Repository</a>
+                            <a>No Deployed Version</a>
+                            <a class="right" href="https://github.com/CIDoubleAgent/My-Team-Profile-Generator" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
                         </div>
                     </div>
                 </article>
