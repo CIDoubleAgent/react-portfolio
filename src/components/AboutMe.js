72d8b73fa456530b8284acdbc074ffb675c67b5a
Nick Zabkar <nzabkar51@gmail.com>
2022-11-29 16:58:39 -0500

    Updated age

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,7 +14,7 @@ function AboutMe() {
                     <img id="profile-image" src="https://github.com/CIDoubleAgent/Nicks-Web-Dev-Portfolio/blob/main/images/profilePicMe_1.jpg?raw=true" alt="Nicholas Zabkar" />
                 </div>
                     <div class="bio flow-text">
-                    I am a 35 year old web developer from Dover, NH and have been a carpenter by trade for most of my career. My hobbies include hiking the many beautiful trails and mountains of New Hampshire, playing basketball, wakeboarding, enjoying craft beer, and poking holes in movie plots.
+                    I am a 36 year old web developer from Dover, NH and have been a carpenter by trade for most of my career. My hobbies include hiking the many beautiful trails and mountains of New Hampshire, playing basketball, wakeboarding, enjoying craft beer, and poking holes in movie plots.
                     </div>
                     <div class="skills flow-text">
                     Skills: I have experience in html, css, javascript ES6+, jQuery, SQL, GraphQL, node.js and React.js.
